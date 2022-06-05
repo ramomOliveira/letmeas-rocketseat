352cb001f82873b1a48165d4e409a20d4f10c9a3
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-06-05 14:49:45 -0300

    firebase tuning

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,6 +7,9 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
