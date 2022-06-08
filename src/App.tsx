dac859169467b5448015c7892ba911a61ab77e16
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-06-07 23:04:53 -0300

    icon like add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
@@ -13,6 +14,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoom />} />
           <Route path="/rooms/:id" element={<Room />} />
+
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
